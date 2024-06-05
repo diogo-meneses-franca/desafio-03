32eee1c298d6997c7c4fc26a261d97160dc4330a
Nicolas <nicolas.marsil02@gmail.com>
2024-06-05 18:14:18 -0300

    feat: método cadastrar em FuncionarioController

--- api-funcionarios/src/main/java/com/pbcompass/apifuncionarios/controller/v1/FuncionarioController.java
+++ api-funcionarios/src/main/java/com/pbcompass/apifuncionarios/controller/v1/FuncionarioController.java
@@ -1,7 +1,16 @@
 package com.pbcompass.apifuncionarios.controller.v1;
 
+import com.pbcompass.apifuncionarios.dto.FuncionarioCadastrarDto;
+import com.pbcompass.apifuncionarios.dto.FuncionarioRespostaDto;
+import com.pbcompass.apifuncionarios.dto.mapper.FuncionarioMapper;
+import com.pbcompass.apifuncionarios.entities.Funcionario;
 import com.pbcompass.apifuncionarios.services.FuncionarioService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +20,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class FuncionarioController {
 
     private final FuncionarioService service;
+
+    @PostMapping
+    public ResponseEntity<FuncionarioRespostaDto> cadastrar(@RequestBody @Valid FuncionarioCadastrarDto dto) {
+        Funcionario funcionario = FuncionarioMapper.toEntity(dto, Funcionario.class);
+        FuncionarioRespostaDto resposta = FuncionarioMapper.toDto(service.cadastrar(funcionario), FuncionarioRespostaDto.class);
+        return ResponseEntity.status(HttpStatus.CREATED).body(resposta);
+    }
+
 }
