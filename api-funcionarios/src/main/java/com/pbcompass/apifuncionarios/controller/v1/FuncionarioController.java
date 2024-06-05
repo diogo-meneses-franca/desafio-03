949ab4e4c3769657d8af13708023ee89eb4f6d85
Diogo Meneses de Franca <m319726@alunos.ipb.pt>
2024-06-05 19:50:35 -0300

    feat: método buscarPorId em FuncionarioController

--- api-funcionarios/src/main/java/com/pbcompass/apifuncionarios/controller/v1/FuncionarioController.java
+++ api-funcionarios/src/main/java/com/pbcompass/apifuncionarios/controller/v1/FuncionarioController.java
@@ -1,7 +1,15 @@
 package com.pbcompass.apifuncionarios.controller.v1;
 
+import com.pbcompass.apifuncionarios.dto.FuncionarioRespostaDto;
+import com.pbcompass.apifuncionarios.dto.mapper.FuncionarioMapper;
+import com.pbcompass.apifuncionarios.entities.Funcionario;
 import com.pbcompass.apifuncionarios.services.FuncionarioService;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +19,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class FuncionarioController {
 
     private final FuncionarioService service;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<FuncionarioRespostaDto> buscarPorId(@PathVariable Long id) {
+        Funcionario funcionario = service.buscarPorId(id);
+        FuncionarioRespostaDto resposta = FuncionarioMapper.toDto(funcionario, FuncionarioRespostaDto.class);
+        return ResponseEntity.status(HttpStatus.OK).body(resposta);
+    }
 }
