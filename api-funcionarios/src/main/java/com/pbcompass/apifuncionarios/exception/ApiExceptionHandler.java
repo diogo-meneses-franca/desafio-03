a4bb7ecc512e0cb7d18a45f97fae700f6b4da26a
Diogo Meneses de Franca <m319726@alunos.ipb.pt>
2024-06-05 17:42:31 -0300

    feat: tratamento de exceção para EntityNotFoundException em ApiExceptionHandler

--- api-funcionarios/src/main/java/com/pbcompass/apifuncionarios/exception/ApiExceptionHandler.java
+++ api-funcionarios/src/main/java/com/pbcompass/apifuncionarios/exception/ApiExceptionHandler.java
@@ -1,11 +1,29 @@
 package com.pbcompass.apifuncionarios.exception;
 
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 @Slf4j
 public class ApiExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<MensagemErroPadrao> entityNotFoundException(EntityNotFoundException e, HttpServletRequest request) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                new MensagemErroPadrao(
+                        System.currentTimeMillis(),
+                        HttpStatus.NOT_FOUND.value(),
+                        "Not found",
+                        e.getMessage(),
+                        request.getRequestURI()
+                )
+        );
+    }
 
 
 }
