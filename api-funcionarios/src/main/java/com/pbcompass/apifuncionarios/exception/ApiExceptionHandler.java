f50fd95f7a1f7c7a7e9ab069a9b2661931d3fd81
Nicolas <nicolas.marsil02@gmail.com>
2024-06-05 18:22:07 -0300

    feat: tratamento dadosDeEntradaInvalidos

--- api-funcionarios/src/main/java/com/pbcompass/apifuncionarios/exception/ApiExceptionHandler.java
+++ api-funcionarios/src/main/java/com/pbcompass/apifuncionarios/exception/ApiExceptionHandler.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -15,6 +16,12 @@ public class ApiExceptionHandler {
     public ResponseEntity<MensagemErroPadrao> cpfUnicoException(CpfUnicoException ex, HttpServletRequest request) {
         return ResponseEntity.status(HttpStatus.CONFLICT).body(new MensagemErroPadrao(System.currentTimeMillis(), 409,
                 "Erro ao cadastrar funcionário", ex.getMessage(), request.getRequestURI()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<MensagemErroPadrao> dadosDeEntradaInvalidosException(MethodArgumentNotValidException ex, HttpServletRequest request) {
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(new MensagemErroPadrao(System.currentTimeMillis(), 422,
+                "Erro ao cadastrar funcionário", "Dados de entrada inválidos", request.getRequestURI()));
     }
 
 }
