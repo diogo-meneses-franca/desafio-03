3c353c0d750bf4e0549b840d93bc4e06969c625a
Nicolas <nicolas.marsil02@gmail.com>
2024-06-05 19:11:17 -0300

    docs: documentação método cadastrar

--- api-funcionarios/src/main/java/com/pbcompass/apifuncionarios/controller/v1/FuncionarioController.java
+++ api-funcionarios/src/main/java/com/pbcompass/apifuncionarios/controller/v1/FuncionarioController.java
@@ -4,7 +4,13 @@ import com.pbcompass.apifuncionarios.dto.FuncionarioCadastrarDto;
 import com.pbcompass.apifuncionarios.dto.FuncionarioRespostaDto;
 import com.pbcompass.apifuncionarios.dto.mapper.FuncionarioMapper;
 import com.pbcompass.apifuncionarios.entities.Funcionario;
+import com.pbcompass.apifuncionarios.exception.MensagemErroPadrao;
 import com.pbcompass.apifuncionarios.services.FuncionarioService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -14,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Funcionários", description = "Contém todas as operações relativas ao cadastro, localização, edição e exclusão de alunos")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/funcionarios")
@@ -21,6 +28,24 @@ public class FuncionarioController {
 
     private final FuncionarioService service;
 
+    @Operation(summary = "Cadastrar um novo funcionário",
+            responses = {
+                    @ApiResponse(
+                            description = "Funcionário cadastrado com sucesso",
+                            responseCode = "201",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = FuncionarioRespostaDto.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "409",
+                            description = "CPF já cadastrado",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = MensagemErroPadrao.class))
+                    ),
+                    @ApiResponse(responseCode = "422",
+                            description = "Dados de entrada inválidos",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = MensagemErroPadrao.class))
+                    )
+            }
+    )
     @PostMapping
     public ResponseEntity<FuncionarioRespostaDto> cadastrar(@RequestBody @Valid FuncionarioCadastrarDto dto) {
         Funcionario funcionario = FuncionarioMapper.toEntity(dto, Funcionario.class);
