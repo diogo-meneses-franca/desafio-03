2b15ea2e52648f238fd26d694b1561c653102405
Giovani Eugenio <giovani.eugenio.sp@gmail.com>
2024-06-05 20:25:16 -0300

    feat: exceptions customizadas

---
+++ api-funcionarios/src/main/java/com/pbcompass/apifuncionarios/exception/custom/ErroAoSalvarFuncionario.java
@@ -0,0 +1,8 @@
+package com.pbcompass.apifuncionarios.exception.custom;
+
+public class ErroAoSalvarFuncionario extends RuntimeException{
+
+    public ErroAoSalvarFuncionario(String msg){
+        super(msg);
+    }
+}
