0ed1779ac16e03685d4edaa2f36fbf725b271bc0
Nicolas <nicolas.marsil02@gmail.com>
2024-06-05 18:15:29 -0300

    feat: tratamento CpfUnicoException

--- api-funcionarios/src/main/java/com/pbcompass/apifuncionarios/exception/ApiExceptionHandler.java
+++ api-funcionarios/src/main/java/com/pbcompass/apifuncionarios/exception/ApiExceptionHandler.java
@@ -1,11 +1,20 @@
 package com.pbcompass.apifuncionarios.exception;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 @Slf4j
 public class ApiExceptionHandler {
 
+    @ExceptionHandler(CpfUnicoException.class)
+    public ResponseEntity<MensagemErroPadrao> cpfUnicoException(CpfUnicoException ex, HttpServletRequest request) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(new MensagemErroPadrao(System.currentTimeMillis(), 409,
+                "Erro ao cadastrar funcionário", ex.getMessage(), request.getRequestURI()));
+    }
 
 }
