6c4dc808b5346a2fa227d452f876af7571d3a78e
Giovani Eugenio <giovani.eugenio.sp@gmail.com>
2024-06-05 20:23:28 -0300

    feat: Exception -> metodos ErroAoSalvarFuncionario e RecursoNaoEncontrado

--- api-funcionarios/src/main/java/com/pbcompass/apifuncionarios/exception/ApiExceptionHandler.java
+++ api-funcionarios/src/main/java/com/pbcompass/apifuncionarios/exception/ApiExceptionHandler.java
@@ -1,11 +1,24 @@
 package com.pbcompass.apifuncionarios.exception;
 
+import com.pbcompass.apifuncionarios.exception.custom.ErroAoSalvarFuncionario;
+import com.pbcompass.apifuncionarios.exception.custom.RecursoNaoEncontrado;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 @Slf4j
 public class ApiExceptionHandler {
 
+    @ExceptionHandler(ErroAoSalvarFuncionario.class)
+    public ResponseEntity<String> erroAoSalvarFuncionario(ErroAoSalvarFuncionario e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+    }
 
+    @ExceptionHandler(RecursoNaoEncontrado.class)
+    public ResponseEntity<String> recursoNaoEncontrado(RecursoNaoEncontrado e){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
 }
