a1d698080e19d5e7f62755767631c8a1add758e9
Nicolas <nicolas.marsil02@gmail.com>
2024-06-07 02:54:48 -0300

    feat: classe de execeção AtualizacaoNaoPermitida e tratamento em ApiExceptionHandler

---
+++ api-funcionarios/src/main/java/com/pbcompass/apifuncionarios/exception/custom/AtualizacaoNaoPermitida.java
@@ -0,0 +1,9 @@
+package com.pbcompass.apifuncionarios.exception.custom;
+
+public class AtualizacaoNaoPermitida extends RuntimeException {
+
+    public AtualizacaoNaoPermitida(String msg) {
+        super(msg);
+    }
+
+}
