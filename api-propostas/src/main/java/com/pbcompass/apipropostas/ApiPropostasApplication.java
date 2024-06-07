9171f62e9cd7359215cb6eedaead6cfe07612197
Diogo Meneses de Franca <m319726@alunos.ipb.pt>
2024-06-07 16:43:12 -0300

    feat: api-propostas

---
+++ api-propostas/src/main/java/com/pbcompass/apipropostas/ApiPropostasApplication.java
@@ -0,0 +1,13 @@
+package com.pbcompass.apipropostas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPropostasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiPropostasApplication.class, args);
+    }
+
+}
---
+++ api-propostas/src/test/java/com/pbcompass/apipropostas/ApiPropostasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pbcompass.apipropostas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPropostasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
