4316ac968f3a8176f4aceea5f06c726b548c4998
Nicolas <nicolas.marsil02@gmail.com>
2024-06-07 01:52:57 -0300

    feat: teste metodo toEntity em FuncionarioMapper

--- api-funcionarios/src/test/java/com/pbcompass/apifuncionarios/FuncionarioMapperTest.java
+++ api-funcionarios/src/test/java/com/pbcompass/apifuncionarios/FuncionarioMapperTest.java
@@ -25,4 +25,16 @@ public class FuncionarioMapperTest {
         assertThat(testeDto.getEmail()).isEqualTo(FUNCIONARIO.getEmail());
     }
 
+    @Test
+    public void toEntity_RetornarFuncionario() {
+
+        Funcionario testeDto = FuncionarioMapper.toEntity(FUNC_CADASTRAR_DTO, Funcionario.class);
+
+        assertThat(testeDto.getNome()).isEqualTo(FUNC_CADASTRAR_DTO.getNome());
+        assertThat(testeDto.getCpf()).isEqualTo(FUNC_CADASTRAR_DTO.getCpf());
+        assertThat(testeDto.getEndereco()).isEqualTo(FUNC_CADASTRAR_DTO.getEndereco());
+        assertThat(testeDto.getTelefone()).isEqualTo(FUNC_CADASTRAR_DTO.getTelefone());
+        assertThat(testeDto.getEmail()).isEqualTo(FUNC_CADASTRAR_DTO.getEmail());
+    }
+
 }
