e2ed04253c6e67fc49e613b163141f6ba0af134e
Diogo Meneses de Franca <m319726@alunos.ipb.pt>
2024-06-08 18:06:35 -0300

    feat: classe PropostaMapper

---
+++ api-propostas/src/main/java/com/pbcompass/apipropostas/dto/mapper/PropostaMapper.java
@@ -0,0 +1,26 @@
+package com.pbcompass.apipropostas.dto.mapper;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.modelmapper.ModelMapper;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class PropostaMapper {
+
+    public static <T, D> D toDto(T entity, Class<D> dtoClass) {
+        return new ModelMapper().map(entity, dtoClass);
+    }
+
+    public static <T, D> T toEntity(D dto, Class<T> entityClass) {
+        return new ModelMapper().map(dto, entityClass);
+    }
+
+    public static <T, D> List<D> toDtoList(List<T> entityList, Class<D> dtoClass) {
+        return entityList.stream()
+                .map(entity -> toDto(entity, dtoClass))
+                .collect(Collectors.toList());
+    }
+}
