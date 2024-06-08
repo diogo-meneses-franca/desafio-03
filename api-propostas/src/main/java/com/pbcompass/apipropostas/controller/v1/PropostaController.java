69e55374f8e81bd58ce716e479853df25500f59e
Nicolas <nicolas.marsil02@gmail.com>
2024-06-08 18:35:15 -0300

    docs: documentacao Swagger metodo buscarTodos

--- api-propostas/src/main/java/com/pbcompass/apipropostas/controller/v1/PropostaController.java
+++ api-propostas/src/main/java/com/pbcompass/apipropostas/controller/v1/PropostaController.java
@@ -1,7 +1,13 @@
 package com.pbcompass.apipropostas.controller.v1;
 
 import com.pbcompass.apipropostas.dto.PropostaRespostaDto;
+import com.pbcompass.apipropostas.exception.MensagemErroPadrao;
 import com.pbcompass.apipropostas.services.PropostaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -20,6 +26,19 @@ public class PropostaController {
 
     private final PropostaService service;
 
+    @Operation(summary = "Busca todas as propostas paginadas",
+            responses = {
+                    @ApiResponse(
+                            description = "Sucesso",
+                            responseCode = "200",
+                            content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = PropostaRespostaDto.class)))
+                    ),
+                    @ApiResponse(responseCode = "500",
+                            description = "Erro inesperado do servidor",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = MensagemErroPadrao.class))
+                    ),
+            }
+    )
     @GetMapping
     public ResponseEntity<Page<PropostaRespostaDto>> buscarTodos(
             @RequestParam(value = "page",defaultValue = "0") Integer page,
