5707cbac022121e0432b4fd60a553301064f96e0
Nicolas <nicolas.marsil02@gmail.com>
2024-06-08 17:56:06 -0300

    feat: metodo buscarTodos em PropostaController e PropostaService

--- api-propostas/src/main/java/com/pbcompass/apipropostas/controller/v1/PropostaController.java
+++ api-propostas/src/main/java/com/pbcompass/apipropostas/controller/v1/PropostaController.java
@@ -1,8 +1,17 @@
 package com.pbcompass.apipropostas.controller.v1;
 
+import com.pbcompass.apipropostas.dto.PropostaRespostaDto;
 import com.pbcompass.apipropostas.repository.PropostaRepository;
+import com.pbcompass.apipropostas.services.PropostaService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +19,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class PropostaController {
 
-    private final PropostaRepository repository;
+    private final PropostaService service;
+
+    @GetMapping
+    public ResponseEntity<Page<PropostaRespostaDto>> buscarTodos(
+            @RequestParam(value = "page",defaultValue = "0") Integer page,
+            @RequestParam(value = "size", defaultValue = "20") Integer size,
+            @RequestParam(value = "direction", defaultValue = "asc") String direction){
+        var sordDirection = "desc".equalsIgnoreCase(direction) ?
+                Sort.Direction.DESC : Sort.Direction.ASC;
+        Pageable pageable = PageRequest.of(page, size, Sort.by(sordDirection, "nome"));
+        return ResponseEntity.ok(service.buscarTodos(pageable));
+    }
+
 }
--- api-propostas/src/main/java/com/pbcompass/apipropostas/services/PropostaService.java
+++ api-propostas/src/main/java/com/pbcompass/apipropostas/services/PropostaService.java
@@ -1,12 +1,25 @@
 package com.pbcompass.apipropostas.services;
 
+import com.pbcompass.apipropostas.dto.PropostaRespostaDto;
+import com.pbcompass.apipropostas.dto.mapper.PropostaMapper;
+import com.pbcompass.apipropostas.entities.Proposta;
 import com.pbcompass.apipropostas.repository.PropostaRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class PropostaService {
 
     private final PropostaRepository repository;
+
+    @Transactional(readOnly = true)
+    public Page<PropostaRespostaDto> buscarTodos(Pageable pageable) {
+        Page<Proposta> proposta = repository.findAll(pageable);
+        Page<PropostaRespostaDto> propostasDto = proposta.map(p -> PropostaMapper.toDto(p, PropostaRespostaDto.class));
+        return propostasDto;
+    }
 }
