7b61d9e9341863d39738b364264a69b8c924270a
Nicolas <nicolas.marsil02@gmail.com>
2024-06-08 18:31:07 -0300

    feat: metodo buscarTodos em PropostaController e PropostaService

--- api-propostas/src/main/java/com/pbcompass/apipropostas/controller/v1/PropostaController.java
+++ api-propostas/src/main/java/com/pbcompass/apipropostas/controller/v1/PropostaController.java
@@ -1,8 +1,16 @@
 package com.pbcompass.apipropostas.controller.v1;
 
+import com.pbcompass.apipropostas.dto.PropostaRespostaDto;
 import com.pbcompass.apipropostas.services.PropostaService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,4 +19,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class PropostaController {
 
     private final PropostaService service;
+
+    @GetMapping
+    public ResponseEntity<Page<PropostaRespostaDto>> buscarTodos(
+            @RequestParam(value = "page",defaultValue = "0") Integer page,
+            @RequestParam(value = "size", defaultValue = "20") Integer size,
+            @RequestParam(value = "direction", defaultValue = "asc") String direction){
+        var sordDirection = "desc".equalsIgnoreCase(direction) ?
+                Sort.Direction.DESC : Sort.Direction.ASC;
+        Pageable pageable = PageRequest.of(page, size, Sort.by(sordDirection, "nome"));
+        return ResponseEntity.ok(service.buscarTodos(pageable));
+    }
+
 }
