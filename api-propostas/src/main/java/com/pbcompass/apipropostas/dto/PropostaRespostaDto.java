293f621e672fdad14f216c9e2fd7912c9ddfcc03
Diogo Meneses de Franca <m319726@alunos.ipb.pt>
2024-06-09 15:12:30 -0300

    feat: adicionados getter, setter, NoArgsContructor e AllArgsConstructor

--- api-propostas/src/main/java/com/pbcompass/apipropostas/dto/PropostaRespostaDto.java
+++ api-propostas/src/main/java/com/pbcompass/apipropostas/dto/PropostaRespostaDto.java
@@ -1,7 +1,18 @@
 package com.pbcompass.apipropostas.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.Date;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class PropostaRespostaDto {
 
     private Long id;
