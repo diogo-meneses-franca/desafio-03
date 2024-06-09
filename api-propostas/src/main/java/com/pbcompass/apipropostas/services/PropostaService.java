019f39929fff05af8cfb4e2a8a16de1d28677d52
Diogo Meneses de Franca <m319726@alunos.ipb.pt>
2024-06-09 19:25:19 -0300

    refactor: removida logica de programação do PropostaController para o PropostaService

--- api-propostas/src/main/java/com/pbcompass/apipropostas/services/PropostaService.java
+++ api-propostas/src/main/java/com/pbcompass/apipropostas/services/PropostaService.java
@@ -3,7 +3,7 @@ package com.pbcompass.apipropostas.services;
 import com.pbcompass.apipropostas.dto.FuncionarioRespostaDto;
 import com.pbcompass.apipropostas.dto.PropostaCadastrarDto;
 import com.pbcompass.apipropostas.dto.PropostaRespostaDto;
-import com.pbcompass.apipropostas.dto.mapper.MapperGenerico;
+import com.pbcompass.apipropostas.services.mapper.MapperGenerico;
 import com.pbcompass.apipropostas.entities.Proposta;
 import com.pbcompass.apipropostas.exception.ErroAoBuscarFuncionarioException;
 import com.pbcompass.apipropostas.feign.FuncionarioFeignClient;
@@ -12,7 +12,9 @@ import com.pbcompass.apipropostas.repository.PropostaRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,13 +27,18 @@ public class PropostaService {
     private final FuncionarioFeignClient feignClient;
 
     @Transactional(readOnly = true)
-    public Proposta buscarPorId(Long id){
-        return repository.findById(id).orElseThrow(
+    public PropostaRespostaDto buscarPorId(Long id){
+        Proposta proposta =  repository.findById(id).orElseThrow(
                 ()-> new RecursoNaoEncontrado(String.format("Proposta com o id %d não encontrada", id)));
+        PropostaRespostaDto resposta = MapperGenerico.toDto(proposta, PropostaRespostaDto.class);
+        return resposta;
     }
 
     @Transactional(readOnly = true)
-    public Page<PropostaRespostaDto> buscarTodos(Pageable pageable) {
+    public Page<PropostaRespostaDto> buscarTodos(Integer page, Integer size, String direction) {
+        var sordDirection = "desc".equalsIgnoreCase(direction) ?
+                Sort.Direction.DESC : Sort.Direction.ASC;
+        Pageable pageable = PageRequest.of(page, size, Sort.by(sordDirection, "nome"));
         Page<Proposta> propostas = repository.findAll(pageable);
         Page<PropostaRespostaDto> propostasDto = propostas.map(f -> MapperGenerico.toDto(f, PropostaRespostaDto.class));
         return propostasDto;
