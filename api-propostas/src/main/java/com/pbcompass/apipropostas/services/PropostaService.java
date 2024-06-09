f09d19506618f13f1ebfb959eb2a7a6ceab51004
Giovani Eugenio <giovani.eugenio.sp@gmail.com>
2024-06-09 09:07:00 -0300

    feat: criado método buscarPorId no service

--- api-propostas/src/main/java/com/pbcompass/apipropostas/services/PropostaService.java
+++ api-propostas/src/main/java/com/pbcompass/apipropostas/services/PropostaService.java
@@ -3,7 +3,9 @@ package com.pbcompass.apipropostas.services;
 import com.pbcompass.apipropostas.dto.PropostaRespostaDto;
 import com.pbcompass.apipropostas.dto.mapper.PropostaMapper;
 import com.pbcompass.apipropostas.entities.Proposta;
+import com.pbcompass.apipropostas.exception.custom.RecursoNaoEncontrado;
 import com.pbcompass.apipropostas.repository.PropostaRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -17,9 +19,16 @@ public class PropostaService {
     private final PropostaRepository repository;
 
     @Transactional(readOnly = true)
+    public Proposta buscarPorId(Long id){
+        return repository.findById(id).orElseThrow(
+                ()-> new RecursoNaoEncontrado(String.format("Proposta com o id %d não encontrada", id)));
+    }
+
+    @Transactional(readOnly = true)
     public Page<PropostaRespostaDto> buscarTodos(Pageable pageable) {
         Page<Proposta> propostas = repository.findAll(pageable);
         Page<PropostaRespostaDto> propostasDto = propostas.map(f -> PropostaMapper.toDto(f, PropostaRespostaDto.class));
         return propostasDto;
     }
+
 }
