cf7cfbe0badfa2e1636475ffa5b07c8ba184fc48
Nicolas <nicolas.marsil02@gmail.com>
2024-06-10 17:39:09 -0300

    refactor: adição de hashcode equals em FuncionarioRespostaDto

--- api-propostas/src/main/java/com/pbcompass/apipropostas/dto/FuncionarioRespostaDto.java
+++ api-propostas/src/main/java/com/pbcompass/apipropostas/dto/FuncionarioRespostaDto.java
@@ -1,14 +1,17 @@
 package com.pbcompass.apipropostas.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @NoArgsConstructor
 @AllArgsConstructor
 public class FuncionarioRespostaDto {
 
+    @EqualsAndHashCode.Include
     private Long id;
     private String nome;
     private String cpf;
