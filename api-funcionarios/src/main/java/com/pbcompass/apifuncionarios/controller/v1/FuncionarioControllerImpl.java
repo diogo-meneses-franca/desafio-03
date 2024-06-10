7d110aa408a731903406f11a2d6a3210bd4676a5
tomazdalcortivo <p12547254@gmail.com>
2024-06-09 21:48:31 -0300

    refactor: logs no controller e metodo buscar por id

--- api-funcionarios/src/main/java/com/pbcompass/apifuncionarios/controller/v1/FuncionarioControllerImpl.java
+++ api-funcionarios/src/main/java/com/pbcompass/apifuncionarios/controller/v1/FuncionarioControllerImpl.java
@@ -1,29 +1,19 @@
 package com.pbcompass.apifuncionarios.controller.v1;
 
 import com.pbcompass.apifuncionarios.dto.FuncionarioRespostaDto;
-import com.pbcompass.apifuncionarios.dto.mapper.FuncionarioMapper;
+import com.pbcompass.apifuncionarios.services.mapper.MapperGenerico;
 import com.pbcompass.apifuncionarios.entities.Funcionario;
 import com.pbcompass.apifuncionarios.dto.FuncionarioCadastrarDto;
-import com.pbcompass.apifuncionarios.exception.custom.MensagemErroPadrao;
 import com.pbcompass.apifuncionarios.services.FuncionarioService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.ArraySchema;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class FuncionarioControllerImpl implements FuncionarioController {
@@ -31,32 +21,28 @@ public class FuncionarioControllerImpl implements FuncionarioController {
     private final FuncionarioService service;
 
     @Override
-    public ResponseEntity<FuncionarioRespostaDto> cadastrar(FuncionarioCadastrarDto dto) {
-        Funcionario funcionario = FuncionarioMapper.toEntity(dto, Funcionario.class);
-        FuncionarioRespostaDto resposta = FuncionarioMapper.toDto(service.cadastrar(funcionario), FuncionarioRespostaDto.class);
-        log.info("Novo funcionário criado.");
-        return ResponseEntity.status(HttpStatus.CREATED).body(resposta);
+    public ResponseEntity<FuncionarioRespostaDto> buscarPorId(Long id) {
+        FuncionarioRespostaDto resposta = service.buscarPorId(id);
+        return ResponseEntity.status(HttpStatus.OK).body(resposta);
     }
 
     @Override
-    public ResponseEntity<FuncionarioRespostaDto> buscarPorId(Long id) {
-        Funcionario funcionario = service.buscarPorId(id);
-        FuncionarioRespostaDto resposta = FuncionarioMapper.toDto(funcionario, FuncionarioRespostaDto.class);
-        return ResponseEntity.status(HttpStatus.OK).body(resposta);
+    public ResponseEntity<FuncionarioRespostaDto> cadastrar(FuncionarioCadastrarDto dto) {
+        Funcionario funcionario = MapperGenerico.toEntity(dto, Funcionario.class);
+        FuncionarioRespostaDto resposta = MapperGenerico.toDto(service.cadastrar(funcionario), FuncionarioRespostaDto.class);
+        return ResponseEntity.status(HttpStatus.CREATED).body(resposta);
     }
 
     @Override
     public ResponseEntity<?> excluir(Long id) {
         service.excluir(id);
-        log.info("Funcionário excluído da base de dados.");
         return ResponseEntity.noContent().build();
     }
 
-
     @Override
     public ResponseEntity<FuncionarioRespostaDto> editar(Long id, FuncionarioCadastrarDto dto) {
-        Funcionario funcionario = FuncionarioMapper.toEntity(dto, Funcionario.class);
-        FuncionarioRespostaDto resposta = FuncionarioMapper.toDto(service.editar(id, funcionario), FuncionarioRespostaDto.class);
+        Funcionario funcionario = MapperGenerico.toEntity(dto, Funcionario.class);
+        FuncionarioRespostaDto resposta = MapperGenerico.toDto(service.editar(id, funcionario), FuncionarioRespostaDto.class);
         return ResponseEntity.ok(resposta);
     }
 
--- api-funcionarios/src/main/java/com/pbcompass/apifuncionarios/services/FuncionarioService.java
+++ api-funcionarios/src/main/java/com/pbcompass/apifuncionarios/services/FuncionarioService.java
@@ -1,7 +1,7 @@
 package com.pbcompass.apifuncionarios.services;
 
 import com.pbcompass.apifuncionarios.dto.FuncionarioRespostaDto;
-import com.pbcompass.apifuncionarios.dto.mapper.FuncionarioMapper;
+import com.pbcompass.apifuncionarios.services.mapper.MapperGenerico;
 import com.pbcompass.apifuncionarios.entities.Funcionario;
 import com.pbcompass.apifuncionarios.exception.custom.DadosUnicosException;
 import com.pbcompass.apifuncionarios.exception.custom.AtualizacaoNaoPermitida;
@@ -9,6 +9,7 @@ import com.pbcompass.apifuncionarios.exception.custom.ErroAoSalvarFuncionario;
 import com.pbcompass.apifuncionarios.repository.FuncionarioRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -16,19 +17,27 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class FuncionarioService {
 
     private final FuncionarioRepository repository;
 
     @Transactional(readOnly = true)
-    public Funcionario buscarPorId(Long id) {
-        return repository.findById(id).orElseThrow(() ->
+    public FuncionarioRespostaDto buscarPorId(Long id) {
+        log.info("Novo funcionário criado.");
+
+        Funcionario funcionario = repository.findById(id).orElseThrow(() ->
                 new EntityNotFoundException(String.format("Funcionario com o id %d não encontrado", id)));
+
+        FuncionarioRespostaDto resposta = MapperGenerico.toDto(funcionario, FuncionarioRespostaDto.class);
+        return resposta;
     }
 
     @Transactional
     public Funcionario cadastrar(Funcionario funcionario) {
+        log.info("Funcionário excluído da base de dados.");
+
         try {
             return repository.save(funcionario);
         } catch (DataIntegrityViolationException e) {
@@ -37,16 +46,20 @@ public class FuncionarioService {
     }
 
     public void excluir(long id) {
-        Funcionario  entidade = repository.findById(id).orElseThrow(() ->
-                new EntityNotFoundException(String.format("funcionários com o id %d não encontrado", id )));
+        log.info("Funcionário excluído da base de dados.");
+
+        Funcionario entidade = repository.findById(id).orElseThrow(() ->
+                new EntityNotFoundException(String.format("funcionários com o id %d não encontrado", id)));
         repository.delete(entidade);
     }
 
     @Transactional
     public Funcionario editar(Long id, Funcionario funcionario) {
+        log.info("Funcionário editado com sucesso");
+
         try {
-            Funcionario entidade = buscarPorId(id);
-            if(!entidade.getCpf().equals(funcionario.getCpf())) {
+            Funcionario entidade = MapperGenerico.toEntity(buscarPorId(id), Funcionario.class);
+            if (!entidade.getCpf().equals(funcionario.getCpf())) {
                 throw new AtualizacaoNaoPermitida("Não é possível alterar o CPF cadastrado");
             }
 
@@ -63,8 +76,10 @@ public class FuncionarioService {
 
     @Transactional(readOnly = true)
     public Page<FuncionarioRespostaDto> buscarTodos(Pageable pageable) {
+        log.info("Funcionários buscados com sucesso");
+
         Page<Funcionario> funcionarios = repository.findAll(pageable);
-        Page<FuncionarioRespostaDto> funcionariosDto = funcionarios.map(f -> FuncionarioMapper.toDto(f, FuncionarioRespostaDto.class));
+        Page<FuncionarioRespostaDto> funcionariosDto = funcionarios.map(f -> MapperGenerico.toDto(f, FuncionarioRespostaDto.class));
         return funcionariosDto;
     }
 }
