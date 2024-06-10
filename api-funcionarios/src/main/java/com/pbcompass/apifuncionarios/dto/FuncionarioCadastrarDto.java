1088036111f73d21fe1d0cb372041b18dd64476c
Diogo Meneses de Franca <m319726@alunos.ipb.pt>
2024-06-10 15:52:44 -0300

    feat: adicionadas validações a classe FuncionarioRespostaDto

--- api-funcionarios/src/main/java/com/pbcompass/apifuncionarios/dto/FuncionarioCadastrarDto.java
+++ api-funcionarios/src/main/java/com/pbcompass/apifuncionarios/dto/FuncionarioCadastrarDto.java
@@ -13,23 +13,23 @@ import org.hibernate.validator.constraints.br.CPF;
 @AllArgsConstructor
 public class FuncionarioCadastrarDto {
 
-    @NotBlank
-    @Size(max = 150)
+    @NotBlank(message = "Nome não deve estar em branco ou nulo")
+    @Size(max = 150, message = "Nome deve ter no máximo 150 caracteres")
     private String nome;
 
-    @Size(min = 11, max = 11)
+    @Size(min = 11, max = 11, message = "CPF deve possui somente números, tamanho minimo e maximo 11 caracteres")
     @CPF
     private String cpf;
 
-    @NotBlank
-    @Size(max = 200)
+    @NotBlank(message = "Endereço não deve estar em branco ou nulo")
+    @Size(max = 150, message = "Endereço deve possui no máximo 150 caracteres")
     private String endereco;
 
-    @NotBlank
-    @Size(max = 14)
+    @NotBlank(message = "Telefone não deve estar em branco ou nulo")
+    @Size(min = 10, max = 14, message = "Telefone deve possuir entre 10 e 14 caracteres")
     private String telefone;
 
-    @NotBlank
+    @NotBlank(message = "Email não deve estar em branco ou nulo")
     private String email;
 
 }
