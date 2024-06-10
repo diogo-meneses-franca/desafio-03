edf87445b62219f0a9aaf6124d2000f198799197
Nicolas <nicolas.marsil02@gmail.com>
2024-06-10 16:22:36 -0300

    feat: adicionadas anotações em PropostaRespostaDto

--- api-propostas/src/main/java/com/pbcompass/apipropostas/dto/PropostaRespostaDto.java
+++ api-propostas/src/main/java/com/pbcompass/apipropostas/dto/PropostaRespostaDto.java
@@ -1,6 +1,8 @@
 package com.pbcompass.apipropostas.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,8 +18,15 @@ import java.util.Date;
 public class PropostaRespostaDto {
 
     private Long id;
+
+    @NotBlank(message = "Nome da proposta não deve ser nulo ou estar em branco")
+    @Size(min = 10, max = 150, message = "Nome da proposta deve ter no minimo 10 e no maximo 150 caracteres")
     private String nome;
+
+    @NotBlank(message = "Descrição nao deve ser nulo ou estar em branco")
+    @Size(max = 200, message = "Descrição deve ter no máximo 200 caracteres")
     private String descricao;
+
     private FuncionarioRespostaDto criador;
     private Integer duracaoEmMinutos;
     private Date inicioVotacao;
