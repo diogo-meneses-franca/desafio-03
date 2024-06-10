5981f66c56e0c00d223f3fcabeb68e052ba626c4
Nicolas <nicolas.marsil02@gmail.com>
2024-06-10 17:51:08 -0300

    cleanup: apagando imports desnecessarios em PropostaControllerImpl

--- api-propostas/src/main/java/com/pbcompass/apipropostas/controller/v1/PropostaControllerImpl.java
+++ api-propostas/src/main/java/com/pbcompass/apipropostas/controller/v1/PropostaControllerImpl.java
@@ -5,7 +5,6 @@ import com.pbcompass.apipropostas.dto.PropostaRespostaDto;
 import com.pbcompass.apipropostas.services.PropostaService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
