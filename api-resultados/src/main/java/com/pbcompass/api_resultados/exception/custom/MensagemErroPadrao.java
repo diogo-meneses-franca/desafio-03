c4def376c3e6db352b90ff5d937d16138fbfc1dc
tomazdalcortivo <p12547254@gmail.com>
2024-06-11 16:01:34 -0300

    feat: exceptions costukmizadas

---
+++ api-resultados/src/main/java/com/pbcompass/api_resultados/exception/custom/MensagemErroPadrao.java
@@ -0,0 +1,21 @@
+package com.pbcompass.api_resultados.exception.custom;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MensagemErroPadrao implements Serializable {
+
+    private Date timestamp;
+    private Integer status;
+    private String message;
+    private String path;
+}
