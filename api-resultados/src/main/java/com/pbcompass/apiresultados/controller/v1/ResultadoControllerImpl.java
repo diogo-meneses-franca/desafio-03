4f411fbc50445ee6a3810074c1618d218588bb67
Nicolas <nicolas.marsil02@gmail.com>
2024-06-11 19:00:51 -0300

    feat: criado metodo buscarTodos em ResultadoControllerImpl

--- api-resultados/src/main/java/com/pbcompass/apiresultados/controller/v1/ResultadoControllerImpl.java
+++ api-resultados/src/main/java/com/pbcompass/apiresultados/controller/v1/ResultadoControllerImpl.java
@@ -3,6 +3,7 @@ package com.pbcompass.apiresultados.controller.v1;
 import com.pbcompass.apiresultados.dto.ResultadoRespostaDto;
 import com.pbcompass.apiresultados.service.ResultadoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +17,13 @@ public class ResultadoControllerImpl implements ResultadoController {
     public ResponseEntity<ResultadoRespostaDto> buscarPorId(Long id) {
         ResultadoRespostaDto resultado = service.buscarPorId(id);
         return ResponseEntity.ok(resultado);
+    }
+
+    @Override
+    public ResponseEntity<Page<ResultadoRespostaDto>> buscarTodos(
+            Integer page,
+            Integer size,
+            String direction) {
+        return ResponseEntity.ok(service.buscarTodos(page, size, direction));
     }
 }
