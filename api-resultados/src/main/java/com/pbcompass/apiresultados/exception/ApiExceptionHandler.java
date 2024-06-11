1135d02ce6ca6f95deb9a21c1febf6952c656800
Nicolas <nicolas.marsil02@gmail.com>
2024-06-11 18:15:41 -0300

    feat: tratamento exceção ErroAoBuscarPropostaException

--- api-resultados/src/main/java/com/pbcompass/apiresultados/exception/ApiExceptionHandler.java
+++ api-resultados/src/main/java/com/pbcompass/apiresultados/exception/ApiExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.pbcompass.apiresultados.exception;
 
+import com.pbcompass.apiresultados.exception.custom.ErroAoBuscarPropostaException;
 import com.pbcompass.apiresultados.exception.custom.MensagemErroPadrao;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
@@ -22,6 +23,17 @@ public class ApiExceptionHandler {
                         new Date(),
                         HttpStatus.INTERNAL_SERVER_ERROR.value(),
                         "Ocorreu um erro inesperado, tente novamente mais tarde!",
+                        request.getRequestURI()));
+    }
+
+    @ExceptionHandler(ErroAoBuscarPropostaException.class)
+    public ResponseEntity<MensagemErroPadrao> erroAoBuscarProposta(ErroAoBuscarPropostaException e, HttpServletRequest request) {
+        log.error(e.getMessage(), e);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
+                new MensagemErroPadrao(
+                        new Date(),
+                        HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                        e.getMessage(),
                         request.getRequestURI()));
     }
 }
