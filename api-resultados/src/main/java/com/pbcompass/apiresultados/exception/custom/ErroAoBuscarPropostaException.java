40b1b827e192b1739c10deb996a40efa96a6a46e
Nicolas <nicolas.marsil02@gmail.com>
2024-06-11 18:15:08 -0300

    feat: classe exceção ErroAoBuscarPropostaException

---
+++ api-resultados/src/main/java/com/pbcompass/apiresultados/exception/custom/ErroAoBuscarPropostaException.java
@@ -0,0 +1,7 @@
+package com.pbcompass.apiresultados.exception.custom;
+
+public class ErroAoBuscarPropostaException extends  RuntimeException{
+    public ErroAoBuscarPropostaException(String mensagem){
+        super(mensagem);
+    }
+}
