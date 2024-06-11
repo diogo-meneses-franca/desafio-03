71494565596e95aa081f0005ba4a24c72e750653
tomazdalcortivo <p12547254@gmail.com>
2024-06-11 15:55:33 -0300

    feat: classe PropostaRespostaDto

---
+++ api-resultados/src/main/java/com/pbcompass/api_resultados/dto/PropostaRespostaDto.java
@@ -0,0 +1,35 @@
+package com.pbcompass.api_resultados.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.util.Date;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class PropostaRespostaDto {
+
+    @NotNull(message = "Id não deve ser nulo")
+    private Long id;
+
+    @NotBlank(message = "Nome da proposta não deve ser nulo ou estar em branco")
+    @Size(min = 10, max = 150, message = "Nome da proposta deve ter no minimo 10 e no maximo 150 caracteres")
+    private String nome;
+
+    @NotBlank(message = "Descrição nao deve ser nulo ou estar em branco")
+    @Size(max = 200, message = "Descrição deve ter no máximo 200 caracteres")
+    private String descricao;
+
+    private Integer duracaoEmMinutos;
+    private Date inicioVotacao;
+}
