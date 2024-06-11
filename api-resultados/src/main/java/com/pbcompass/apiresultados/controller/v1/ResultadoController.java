a8bbe91420a9bb6ca359e4ab19a0a16c78bbc631
Nicolas <nicolas.marsil02@gmail.com>
2024-06-11 19:03:58 -0300

    feat: criado metodo buscarTodos em ResultadoController

--- api-resultados/src/main/java/com/pbcompass/apiresultados/controller/v1/ResultadoController.java
+++ api-resultados/src/main/java/com/pbcompass/apiresultados/controller/v1/ResultadoController.java
@@ -1,16 +1,24 @@
 package com.pbcompass.apiresultados.controller.v1;
 
-import com.pbcompass.apiresultados.dto.PropostaRespostaDto;
 import com.pbcompass.apiresultados.dto.ResultadoRespostaDto;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RequestMapping("/api/v1/resultados")
 public interface ResultadoController {
 
     @GetMapping("/{id}")
     ResponseEntity<ResultadoRespostaDto> buscarPorId(@PathVariable Long id);
+
+    @GetMapping
+    ResponseEntity<Page<ResultadoRespostaDto>> buscarTodos(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "size", defaultValue = "20") Integer size,
+            @RequestParam(value = "direction", defaultValue = "asc") String direction
+    );
 
 }
