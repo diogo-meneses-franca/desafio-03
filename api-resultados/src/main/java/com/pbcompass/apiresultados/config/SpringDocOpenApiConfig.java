920927358105ea5a62ead5654cc88087551f8bb1
Nicolas <nicolas.marsil02@gmail.com>
2024-06-11 17:14:27 -0300

    config: classe SpringDocOpenApiConfig

---
+++ api-resultados/src/main/java/com/pbcompass/apiresultados/config/SpringDocOpenApiConfig.java
@@ -0,0 +1,22 @@
+package com.pbcompass.apiresultados.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDocOpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(
+                        new Info()
+                                .title("REST API - Resultados")
+                                .description("API para consulta de resultados.")
+                                .version("v1")
+                );
+    }
+
+}
