9b6427335f9d44132099e72cdfba1eb1abfb2c86
Nicolas <nicolas.marsil02@gmail.com>
2024-06-11 18:39:12 -0300

    feat: criado metodo buscarPorId em ResultadoController

--- api-resultados/src/main/java/com/pbcompass/apiresultados/controller/v1/ResultadoController.java
+++ api-resultados/src/main/java/com/pbcompass/apiresultados/controller/v1/ResultadoController.java
@@ -1,7 +1,16 @@
 package com.pbcompass.apiresultados.controller.v1;
 
+import com.pbcompass.apiresultados.dto.PropostaRespostaDto;
+import com.pbcompass.apiresultados.dto.ResultadoRespostaDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/api/v1/resultados")
 public interface ResultadoController {
+
+    @GetMapping("/{id}")
+    ResponseEntity<ResultadoRespostaDto> buscarPorId(@PathVariable Long id);
+
 }
