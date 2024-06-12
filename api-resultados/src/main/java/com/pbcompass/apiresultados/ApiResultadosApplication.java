5bf06294dfe62e2b55c5a938ca7dcfa8fad0283d
Nicolas <nicolas.marsil02@gmail.com>
2024-06-12 17:22:42 -0300

    feat: adição de anotação na classe principal do projeto

--- api-resultados/src/main/java/com/pbcompass/apiresultados/ApiResultadosApplication.java
+++ api-resultados/src/main/java/com/pbcompass/apiresultados/ApiResultadosApplication.java
@@ -2,8 +2,10 @@ package com.pbcompass.apiresultados;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class ApiResultadosApplication {
 
 	public static void main(String[] args) {
