c4e10d1a27da26f0dff5872d12061adabc222aa7
Diogo Meneses de Franca <m319726@alunos.ipb.pt>
2024-06-12 18:05:21 -0300

    feat: classe ResultadoFeignClient

--- api-propostas/src/main/java/com/pbcompass/apipropostas/feign/ResultadoFeignClient.java
+++ api-propostas/src/main/java/com/pbcompass/apipropostas/feign/ResultadoFeignClient.java
@@ -1,2 +1,14 @@
-package com.pbcompass.apipropostas.feign;public class ResultadoFeignClient {
+package com.pbcompass.apipropostas.feign;
+
+import com.pbcompass.apipropostas.dto.ResultadoCadastrarDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "api-resultados", url = "http://localhost:8080/v1/resultados")
+public interface ResultadoFeignClient {
+
+    @PostMapping
+    ResponseEntity<Void> cadastrar(@RequestBody ResultadoCadastrarDto dto);
 }
